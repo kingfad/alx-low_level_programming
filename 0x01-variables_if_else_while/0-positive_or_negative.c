c02caa8591f583a26519846028817534d3118667
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-03-15 10:48:51 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,16 +9,16 @@
  */
 int main(void)
 {
-README.md int n;
+	int n;
 
-README.md srand(time(0));
-README.md n = rand() - RAND_MAX / 2;
-README.md if (n > 0)
-README.md README.md printf("%i is positive\n", n);
-README.md else if (n < 0)
-README.md README.md printf("%i is negative\n", n);
-README.md else
-README.md README.md printf("%i is zero\n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
 
-README.md return (0);
+	return (0);
 }
