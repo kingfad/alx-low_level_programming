8c2956ffa1312212ec080fbbaf5406682d4832af
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-03-17 05:56:46 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,25 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabet 10 times
- * Return:void
+ * _islower - check for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
  */
 
-void print_alphabet_x10(void)
+int _islower(int c)
 {
-	char c;
-	int i = 0;
-
-	while (i < 10)
-	{
-		c = 'a';
-		while (c <= 'z')
-		{
-			_putchar(c);
-			c++;
-		}
-		_putchar('\n');
-		i++;
-	}
+	return (c >= 97 && c <= 122);
 
 }
