8014a73793644a2d25dbae4d4eaedff8debf49d7
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-03-19 05:28:57 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,23 @@
-bin/bash
+#include <stdio.h>
+
+/**
+ * main - prime numbers
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int x = 612852475143;
+	long int py;
+
+	for (py = 2; py < x; py++)
+	{
+		if (x % py == 0)
+		{
+			x = x / py;
+		}
+	}
+	printf("%ld\n", py);
+	return (0);
+}
