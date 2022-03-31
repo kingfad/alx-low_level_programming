c71eaab9626e9ad65d734621560bbc01b019c71a
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-01 00:24:10 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,15 @@
-binbash
+#include "main.h"
+/**
+* _puts_recursion - function prints a string
+* @s: string to be printed
+* Return: function returns nothing
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
