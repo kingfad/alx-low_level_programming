8cd0b2d6f6ab3a3fa75b507df5134dd93eaf0595
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-02 13:00:41 +0100

    Update main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,11 @@
-binbash
+#ifndef MAIN_H
+#define MAIN_H
+/**
+* main - main function's and header file for prototypes
+* void prototypes (void)
+* int prototypes (int)
+*/
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+#endif
