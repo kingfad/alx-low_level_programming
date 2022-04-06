de20f726ee11a2b8f949611c980b55580b406659
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-06 07:10:11 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,20 @@
-binbash
+#include <stdlib.h>
+
+/**
+ * free_grid - alloc_grid
+ * @grid: pointer to 2D arrays
+ * @height: array
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	height--;
+
+	while (height >= 0)
+	{
+		free(*(grid + height));
+		height--;
+	}
+	free(grid);
+}
