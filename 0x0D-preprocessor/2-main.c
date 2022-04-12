0e3493f45a0ae128676663bfe23b4fd0c3bd6bf3
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-12 06:28:15 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,13 @@
-binbash
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
