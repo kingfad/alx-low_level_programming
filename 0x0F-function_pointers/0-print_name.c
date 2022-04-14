8c953319dd41d2b3d88347b34a45a351f764ca12
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-14 05:38:27 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,15 @@
-binbash
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name's main
+ * @f: void function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
