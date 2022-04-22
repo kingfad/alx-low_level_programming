01bc6f4f235b832a20c68f44cfbfdf3bab406ea5
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-22 05:42:51 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,12 @@
-binbash
+#include <stdio.h>
+void first(void) __attribute__ ((constructor));
+
+/**
+* first - function that prints before main is executed
+*/
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
