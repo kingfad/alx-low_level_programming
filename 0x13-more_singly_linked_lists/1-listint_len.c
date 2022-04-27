2d7a1a4be1a0f8ac13386424266ab176481af341
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-27 05:39:45 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,22 @@
-binbash
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* listint_len - function returns the number of elements in a linked list
+* @h: head node
+* Return: Returns node count
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t node_count = 0;
+	const listint_t *temp = h;
+
+	while (temp)
+	{
+		node_count++;
+		temp = temp->next;
+	}
+	return (node_count);
+}
