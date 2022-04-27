d591331b0571231e7b5be809028fed14558fea6b
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-27 05:43:20 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,1 +1,41 @@
-binbash
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* find_listint_loop - function finds the loop in a linked list
+* @head: Pointer to the head node
+* Return: The address of the node where the loop starts
+* or NULL if there is no loop
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *temp1, *temp2;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	temp1 = head->next;
+	temp2 = (head->next)->next;
+
+	while (temp2)
+	{
+		if (temp1 == temp2)
+		{
+			temp1 = head;
+
+			while (temp1 != temp2)
+			{
+				temp1 = temp1->next;
+				temp2 = temp2->next;
+			}
+
+			return (temp1);
+		}
+
+		temp1 = temp1->next;
+		temp2 = (temp2->next)->next;
+	}
+	return (NULL);
+}
