1fcbbfc4ad94c7e5ec3e2b8010825e4b0c2793db
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-29 23:21:37 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,16 @@
-binbash
+#include "main.h"
+
+/**
+* get_endianness - function checks the endianness
+* Return: 0 if big endian, 1 if little endian
+*/
+
+int get_endianness(void)
+{
+	int n = 1;
+	char *endian = (char *)&n;
+
+	if (*endian == 1)
+		return (1);
+	return (0);
+}
