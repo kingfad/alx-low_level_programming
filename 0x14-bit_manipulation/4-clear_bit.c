bf0c3661589f94f60eb9c295bf0e9bbac080f8ee
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-29 23:29:37 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,1 +1,14 @@
-binbash
+#include "main.h"
+
+/**
+* clear_bit - function sets then value of a bit to 0 at a given index
+* @n: Pointer to the number
+* @index: The index starting from 0 of the bit you want to set
+* Return: 1 if successful or -1 if error occurred
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n & ~(1 << index);
+	return ((index > 8 * sizeof(*n)) ? -1 : 1);
+}
