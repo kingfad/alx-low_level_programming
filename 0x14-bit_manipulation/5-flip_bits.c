c9acf856cfeed20e059035b50e49a14453d26f2e
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-29 23:31:57 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,1 +1,20 @@
-binbash
+#include "main.h"
+
+/**
+* flip_bits - function flips the bits to get from one number to another
+* @n: The number to be flipped
+* @m: The number of bits to be flipped
+* Return: The number of bits to be flipped to get from one number to another
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m, bits = 0;
+
+	while (xor > 0)
+	{
+		bits += xor & 1;
+		xor >>= 1;
+	}
+	return (bits);
+}
