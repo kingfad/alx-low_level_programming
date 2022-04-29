819b3ab4aecf432422309265a50fe865dd4d1601
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-29 23:19:34 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,14 @@
-binbash
+#include "main.h"
+
+/**
+* print_binary - function prints binary representation of a number
+* @n: The number to be converted
+* Return: Nothing
+*/
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
